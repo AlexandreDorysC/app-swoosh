318a8662d142ecbe781ec81fb0650433d0ad8bee
Alexandre Dorys-Charnalet <alexandre@macbook-pro-de-alexandre.home>
2017-10-29 20:32:11 +0100

    Passing data between VC

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Alexandre Dorys-Charnalet on 29/10/2017.
+//  Copyright © 2017 Alexandre Dorys-Charnalet. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+}
